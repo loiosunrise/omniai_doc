76a364970fa539b254ab67e05e63ded967de5987
panda <jiaxicui446@gmail.com>
2024-12-18 18:07:31 +0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,9 +11,9 @@ export default defineConfig({
 				discord: 'https://discord.gg/DSGHSMCv2H'
 			},
 			sidebar: [
-				{ label: '介绍', link: '/introduction' },
-				{ label: '使用指导', link: '/guide' },
-				{ label: '计费方式与模型定价', link: '/model_price' },
+				{ label: '介绍', link: '/introduction/' },
+				{ label: '使用指导', link: '/guide/' },
+				{ label: '计费方式与模型定价', link: '/model_price/' },
 			],
 		}),
 	],
